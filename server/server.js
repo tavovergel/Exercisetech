8cfb1fad68f54d4ab9aac0862a44824ebc7d9871
GustavoVergel <tavovergel@gmail.com>
2024-08-14 22:47:29 -0500

    commit8

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
-// server.js
 const express = require('express');
 const { Pool } = require('pg');
+const path = require('path');
+
 const app = express();
 
 // Configura la conexión a la base de datos
@@ -10,53 +11,39 @@ const pool = new Pool({
 
 // Middleware para analizar el cuerpo de las solicitudes JSON
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '../public')));
 
 // Función para ejecutar las consultas SQL
-async function runQuery(query) {
+async function runQuery(query, params) {
   const client = await pool.connect();
   try {
-    const res = await client.query(query);
+    const res = await client.query(query, params);
     return res;
   } finally {
     client.release();
   }
 }
 
-// Crear la tabla e insertar los usuarios si no existen
-const createTableQuery = `
-CREATE TABLE IF NOT EXISTS usuarios (
-    id SERIAL PRIMARY KEY,
-    nombre_usuario VARCHAR(50) NOT NULL,
-    contrasena VARCHAR(255) NOT NULL
-);
-
-INSERT INTO usuarios (nombre_usuario, contrasena)
-VALUES 
-    ('user1', '12345'),
-    ('user2', '789456')
-ON CONFLICT (nombre_usuario) DO NOTHING;  // Evita errores si el usuario ya existe
-`;
-
-runQuery(createTableQuery)
-  .then(() => console.log('Tabla usuarios creada e inicializada con datos'))
-  .catch(err => console.error('Error ejecutando la consulta:', err));
-
 // Endpoint para el login
 app.post('/login', async (req, res) => {
   const { nombre_usuario, contrasena } = req.body;
-  const query = `SELECT * FROM usuarios WHERE nombre_usuario = $1 AND contrasena = $2`;
+  const query = 'SELECT * FROM usuarios WHERE nombre_usuario = $1 AND contrasena = $2';
 
   try {
-    const result = await pool.query({ text: query, values: [nombre_usuario, contrasena] });
+    const result = await pool.query(query, [nombre_usuario, contrasena]);
     if (result.rows.length > 0) {
       res.json({ mensaje: 'Login exitoso', usuario: result.rows[0] });
     } else {
       res.status(401).json({ mensaje: 'Credenciales incorrectas' });
     }
   } catch (err) {
-    res.status(500).json({ error: 'Error en la base de datos' });
+    console.error('Error en la base de datos:', err);
+    res.status(500).json({ mensaje: 'Error en la base de datos' });
   }
 });
+
+// Servir archivos estáticos desde el directorio 'public'
+app.use(express.static(path.join(__dirname, '../public')));
 
 // Inicia el servidor
 const port = process.env.PORT || 3000;
